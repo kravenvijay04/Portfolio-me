b3bf52c1fd4e7f79ddabfaaa276c5b70e00009bc
kravenvijay04 <vijaykumarnr04@gmail.com>
2025-01-28 01:38:35 +0530

    lib folder is added

---
+++ portfolio/lib/constants.ts
@@ -0,0 +1,27 @@
+export const skills = {
+    theObvious: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "git",
+      "npm / yarn",
+      "Rest APIs / JSON",
+      "React Hooks",
+      "fetch / axios"
+    ],
+    librariesFrameworks: [
+      "React 18",
+      "Tailwind",
+      "Redux",
+      "Express js",
+      "Angular",
+      "Node js",
+      "Mongoose"
+    ],
+    toolsOfTheTrade: [
+      "Github",
+      "Figma",
+      "Vercel",
+      "VS code"
+    ]
+  };
